542a8fee0fcdaa6cc01117ecda80e01c999126ad
Aleksandra Glesaaen <aleksandra@glesaaen.com>
2022-08-26 13:21:05 +0200

    fix: default array for setup function
    Refs: fixes #3

--- lua/barbecue/init.lua
+++ lua/barbecue/init.lua
@@ -7,6 +7,10 @@ local M = {}
 ---Configures and starts the plugin
 ---@param config table
 M.setup = function(config)
+  if config == nil then
+    config = {}
+  end
+  
   -- Merges `config` into `default_config` (prefres `config`)
   state.config = vim.tbl_deep_extend("force", state.default_config, config)
 
