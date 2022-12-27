8eb38efbbe8ac37201d8c07842fcd12cf846d4a8
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-27 18:47:49 +0330

    docs(default): define barbecue.Theme class

--- lua/barbecue/theme/default.lua
+++ lua/barbecue/theme/default.lua
@@ -1,38 +1,66 @@
-local M = {}
+---@alias barbecue.ThemeItem.name string
 
-M.normal = { bg = "none", fg = "#ffffff" }
-M.modified = { fg = "#ffff00" }
-M.ellipsis = M.normal
-M.separator = M.normal
-M.dirname = M.normal
-M.basename = M.normal
-M.context = M.normal
+---@class barbecue.ThemeItem.value
+---@field public foreground string
+---@field public background string
+---@field public special string
+---@field public blend number
+---@field public bold boolean
+---@field public standout boolean
+---@field public underline boolean
+---@field public undercurl boolean
+---@field public underdouble boolean
+---@field public underdotted boolean
+---@field public underdashed boolean
+---@field public strikethrough boolean
+---@field public italic boolean
+---@field public reverse boolean
+---@field public nocombine boolean
+---@field public link string
+---@field public default boolean
+---@field public ctermfg number
+---@field public ctermbg number
+---@field public cterm string[]
 
-M.context_file = {}
-M.context_module = {}
-M.context_namespace = {}
-M.context_package = {}
-M.context_class = {}
-M.context_method = {}
-M.context_property = {}
-M.context_field = {}
-M.context_constructor = {}
-M.context_enum = {}
-M.context_interface = {}
-M.context_function = {}
-M.context_variable = {}
-M.context_constant = {}
-M.context_string = {}
-M.context_number = {}
-M.context_boolean = {}
-M.context_array = {}
-M.context_object = {}
-M.context_key = {}
-M.context_null = {}
-M.context_enum_member = {}
-M.context_struct = {}
-M.context_event = {}
-M.context_operator = {}
-M.context_type_parameter = {}
+---@alias barbecue.Theme table<barbecue.ThemeItem.name, barbecue.ThemeItem.value>
+---@type barbecue.Theme
+local M = {
+  normal = { background = "none", foreground = "#ffffff" },
+
+  ellipsis = {},
+  separator = {},
+  modified = { foreground = "#ffff00" },
+
+  dirname = {},
+  basename = {},
+  context = {},
+
+  context_file = {},
+  context_module = {},
+  context_namespace = {},
+  context_package = {},
+  context_class = {},
+  context_method = {},
+  context_property = {},
+  context_field = {},
+  context_constructor = {},
+  context_enum = {},
+  context_interface = {},
+  context_function = {},
+  context_variable = {},
+  context_constant = {},
+  context_string = {},
+  context_number = {},
+  context_boolean = {},
+  context_array = {},
+  context_object = {},
+  context_key = {},
+  context_null = {},
+  context_enum_member = {},
+  context_struct = {},
+  context_event = {},
+  context_operator = {},
+  context_type_parameter = {},
+}
 
 return M
