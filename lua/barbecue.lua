d47ec009ad5df3759f6e0216950256aee8bd653d
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-02 23:34:15 +0330

    refactor(theme): extract autocmd creation to its own module

--- lua/barbecue.lua
+++ lua/barbecue.lua
@@ -1,48 +1,10 @@
-local navic = require("nvim-navic")
 local config = require("barbecue.config")
 local theme = require("barbecue.theme")
 local ui = require("barbecue.ui")
 local utils = require("barbecue.utils")
+local autocmd = require("barbecue.autocmd")
 
 local M = {}
-
----attaches navic to capable language servers on initialization
-local function attach_navic()
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("barbecue#attach_navic", {}),
-    callback = function(a)
-      local client = vim.lsp.get_client_by_id(a.data.client_id)
-      if client.server_capabilities["documentSymbolProvider"] then
-        navic.attach(client, a.buf)
-      end
-    end,
-  })
-end
-
----creates the main autocmd
-local function create_autocmd()
-  local events = {
-    "WinScrolled",
-    "BufWinEnter",
-    "CursorMoved",
-    "InsertLeave",
-  }
-
-  if config.user.show_modified then
-    utils.tbl_merge(events, {
-      "BufWritePost",
-      "TextChanged",
-      "TextChangedI",
-    })
-  end
-
-  vim.api.nvim_create_autocmd(events, {
-    group = vim.api.nvim_create_augroup("barbecue#create_autocmd", {}),
-    callback = function()
-      ui.update()
-    end,
-  })
-end
 
 ---configures and starts barbecue
 ---@param cfg barbecue.Config?
@@ -50,15 +12,9 @@ function M.setup(cfg)
   config.apply(cfg or {})
   theme.load()
 
-  vim.api.nvim_create_autocmd("ColorScheme", {
-    group = vim.api.nvim_create_augroup("barbecue#colorscheme", {}),
-    callback = function()
-      theme.load()
-    end,
-  })
-
-  if config.user.attach_navic then attach_navic() end
-  if config.user.create_autocmd then create_autocmd() end
+  autocmd.create_colorscheme_syncer()
+  if config.user.attach_navic then autocmd.create_navic_attacher() end
+  if config.user.create_autocmd then autocmd.create_updater() end
 
   utils.create_user_command("Barbecue", {
     hide = function()
---
+++ lua/barbecue/autocmd.lua
@@ -0,0 +1,68 @@
+local navic = require("nvim-navic")
+local config = require("barbecue.config")
+local ui = require("barbecue.ui")
+local utils = require("barbecue.utils")
+local theme = require("barbecue.theme")
+
+local M = {}
+
+local PREFIX = "barbecue"
+
+---attaches navic to capable LSPs on their initialization
+function M.create_navic_attacher()
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup(
+      string.format("%s#attach_navic", PREFIX),
+      {}
+    ),
+    callback = function(a)
+      local client = vim.lsp.get_client_by_id(a.data.client_id)
+      if client.server_capabilities["documentSymbolProvider"] then
+        navic.attach(client, a.buf)
+      end
+    end,
+  })
+end
+
+---updates winbar on necessary events
+function M.create_updater()
+  local events = {
+    "WinScrolled",
+    "BufWinEnter",
+    "CursorMoved",
+    "InsertLeave",
+  }
+
+  if config.user.show_modified then
+    utils.tbl_merge(events, {
+      "BufWritePost",
+      "TextChanged",
+      "TextChangedI",
+    })
+  end
+
+  vim.api.nvim_create_autocmd(events, {
+    group = vim.api.nvim_create_augroup(
+      string.format("%s#create_autocmd", PREFIX),
+      {}
+    ),
+    callback = function()
+      ui.update()
+    end,
+  })
+end
+
+---keeps the theme in sync with the current colorscheme
+function M.create_colorscheme_syncer()
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    group = vim.api.nvim_create_augroup(
+      string.format("%s#colorscheme", PREFIX),
+      {}
+    ),
+    callback = function()
+      theme.load()
+    end,
+  })
+end
+
+return M
