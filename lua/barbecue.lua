3002eec57ea8bceadbcfea8ded5d1b34fe6c3bce
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-03 22:52:35 +0330

    refactor(utils): move create_user_command to barbecue module
    Also rename it to create_barbecue_command.

--- lua/barbecue.lua
+++ lua/barbecue.lua
@@ -1,10 +1,49 @@
 local config = require("barbecue.config")
 local theme = require("barbecue.theme")
 local ui = require("barbecue.ui")
-local utils = require("barbecue.utils")
 local autocmd = require("barbecue.autocmd")
 
 local M = {}
+
+---creates user command named `name` and defines subcommands according to `actions`
+---@param actions table<string, fun()>
+local function create_barbecue_command(actions)
+  local subcommands = vim.tbl_keys(actions)
+
+  vim.api.nvim_create_user_command("Barbecue", function(params)
+    if #params.fargs < 1 then
+      local msg = "Available subcommands:"
+      for _, subcommand in ipairs(subcommands) do
+        msg = string.format("%s\n  - %s", msg, subcommand)
+      end
+
+      vim.notify(msg, vim.log.levels.INFO)
+      return
+    end
+
+    local subcommand = params.fargs[1]
+    local action = actions[subcommand]
+    if action == nil then
+      vim.notify(
+        string.format("'%s' is not a subcommand", subcommand),
+        vim.log.levels.ERROR
+      )
+      return
+    end
+
+    action()
+  end, {
+    nargs = "*",
+    complete = function(_, line)
+      local args = vim.split(line, "%s+")
+      if #args ~= 2 then return {} end
+
+      return vim.tbl_filter(function(subcommand)
+        return vim.startswith(subcommand, args[2])
+      end, subcommands)
+    end,
+  })
+end
 
 ---configures and starts barbecue
 ---@param cfg barbecue.Config?
@@ -16,7 +55,7 @@ function M.setup(cfg)
   if config.user.attach_navic then autocmd.create_navic_attacher() end
   if config.user.create_autocmd then autocmd.create_updater() end
 
-  utils.create_user_command("Barbecue", {
+  create_barbecue_command({
     hide = function()
       ui.toggle(false)
     end,
