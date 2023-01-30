d8dad18c7d2ea96099dc5d4712761433f4f551a7
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-30 23:12:26 +0330

    refactor(mouse): interact with nvim APIs directly

--- lua/barbecue/ui/mouse.lua
+++ lua/barbecue/ui/mouse.lua
@@ -1,12 +1,5 @@
 local M = {}
 
----moves the cursor to `to.win` and puts it on `to.pos`
----@param to barbecue.Entry.to
-function M.navigate(to)
-  vim.api.nvim_set_current_win(to.win)
-  vim.api.nvim_win_set_cursor(to.win, to.pos)
-end
-
 setmetatable(M, {
   __index = function(self, key)
     if type(key) ~= "string" then return nil end
@@ -18,7 +11,9 @@ setmetatable(M, {
 
       return function(_, _, button)
         if button ~= "l" then return end
-        self.navigate({ win = win, pos = pos })
+
+        vim.api.nvim_set_current_win(win)
+        vim.api.nvim_win_set_cursor(win, pos)
       end
     end
 
