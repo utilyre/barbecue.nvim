e5493a960bf88f1319414ce993b079ddb6b09454
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-01 17:18:23 +0330

    feat(utils): implement the since_nvim function

--- lua/barbecue/utils.lua
+++ lua/barbecue/utils.lua
@@ -31,4 +31,28 @@ function M.tbl_merge(list, ...)
   end
 end
 
+---Checks whether the current neovim version surpasses the given version.
+---
+---See: [Semantic Versioning](https://semver.org)
+---
+---@param major number
+---@param minor number?
+---@param patch number?
+---@return boolean
+function M.since_nvim(major, minor, patch)
+  minor = minor or 0
+  patch = patch or 0
+
+  local v = vim.version()
+  if v.major > major then return true end
+  if v.major == major then
+    if v.minor > minor then return true end
+    if v.minor == minor then
+      if v.patch >= patch then return true end
+    end
+  end
+
+  return false
+end
+
 return M
