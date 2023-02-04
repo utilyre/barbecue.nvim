345e77172c1ecb120646520061bbbb56bcb1070b
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-04 22:54:16 +0330

    docs(autocmd): add desc field for autocommands

--- lua/barbecue/autocmd.lua
+++ lua/barbecue/autocmd.lua
@@ -13,6 +13,7 @@ local GROUP_COLORSCHEME_SYNCHRONIZER = "barbecue.colorscheme_synchronizer"
 ---Attach navic to capable LSPs on their initialization.
 function M.create_navic_attacher()
   vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "Navic Attacher",
     group = vim.api.nvim_create_augroup(GROUP_NAVIC_ATTACHER, {}),
     callback = function(a)
       local client = vim.lsp.get_client_by_id(a.data.client_id)
@@ -41,6 +42,7 @@ function M.create_updater()
   end
 
   vim.api.nvim_create_autocmd(events, {
+    desc = "Updater",
     group = vim.api.nvim_create_augroup(GROUP_UPDATER, {}),
     callback = function() ui.update() end,
   })
@@ -49,6 +51,7 @@ end
 ---Keep the theme in sync with the current colorscheme.
 function M.create_colorscheme_synchronizer()
   vim.api.nvim_create_autocmd("ColorScheme", {
+    desc = "Colorscheme Synchronizer",
     group = vim.api.nvim_create_augroup(GROUP_COLORSCHEME_SYNCHRONIZER, {}),
     callback = function() theme.load() end,
   })
